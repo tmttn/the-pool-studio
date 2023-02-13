a4829fb8815b55b3b5b393dc18d47e20ffa36299
Thomas Metten <t.metten@aca-it.be>
2023-02-13 08:43:18 +0100

    add gatsby-plugin-gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: 'gatsby-plugin-preconnect',
       options: {
